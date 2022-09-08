08bc52fe96cb5df8fcffda8133db8df7fcf6fe8e
Sami Tolvanen <samitolvanen@google.com>
2022-09-08 14:54:45 -0700

    BACKPORT: cfi: Remove CONFIG_CFI_CLANG_SHADOW
    In preparation to switching to -fsanitize=kcfi, remove support for the
    CFI module shadow that will no longer be needed.
    
    Change-Id: I0ec358739dac185369098db6f56dc71ee36d5698
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Tested-by: Kees Cook <keescook@chromium.org>
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20220908215504.3686827-4-samitolvanen@google.com
    (cherry picked from commit 9fca7115827b2e5f48d84e50bceb4edfd4cb6375)
    Signed-off-by: milxnaq <dupazlasu66@gmail.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- kernel/module.c
+++ kernel/module.c
@@ -2209,8 +2209,6 @@ void __weak module_arch_freeing_init(struct module *mod)
 {
 }
 
-static void cfi_cleanup(struct module *mod);
-
 /* Free a module, remove from lists, etc. */
 static void free_module(struct module *mod)
 {
@@ -2249,9 +2247,6 @@ static void free_module(struct module *mod)
 	/* Wait for RCU-sched synchronizing before releasing mod->list and buglist. */
 	synchronize_rcu();
 	mutex_unlock(&module_mutex);
-
-	/* Clean up CFI for the module. */
-	cfi_cleanup(mod);
 
 	/* This may be empty, but that's OK */
 	module_arch_freeing_init(mod);
@@ -4176,7 +4171,6 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	synchronize_rcu();
 	kfree(mod->args);
  free_arch_cleanup:
-	cfi_cleanup(mod);
 	module_arch_cleanup(mod);
  free_modinfo:
 	free_modinfo(mod);
@@ -4541,14 +4535,6 @@ static void cfi_init(struct module *mod)
 	if (exit)
 		mod->exit = *exit;
 
-	cfi_module_add(mod, module_addr_min);
-#endif
-}
-
-static void cfi_cleanup(struct module *mod)
-{
-#ifdef CONFIG_CFI_CLANG
-	cfi_module_remove(mod, module_addr_min);
 #endif
 }
 
