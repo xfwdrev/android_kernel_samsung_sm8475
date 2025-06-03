371d6dc25f8f62e9f9bbb1367ab894c6e004f363
AKM <73632243+akm-04@users.noreply.github.com>
2025-06-03 15:43:05 +0600

    Add More Power Efficient Workqueue's
    Signed-off-by: akm-04 <akmabrar04@gmail.com>

--- drivers/base/devcoredump.c
+++ drivers/base/devcoredump.c
@@ -389,7 +389,7 @@ void dev_coredumpm(struct device *dev, struct module *owner,
 	mutex_lock(&devcd->mutex);
 	devcd->init_completed = false;
 	INIT_DELAYED_WORK(&devcd->del_wk, devcd_del);
-	schedule_delayed_work(&devcd->del_wk, DEVCD_TIMEOUT);
+	queue_delayed_work(system_power_efficient_wq, &devcd->del_wk, DEVCD_TIMEOUT);
 
 	if (device_add(&devcd->devcd_dev))
 		goto put_device;
