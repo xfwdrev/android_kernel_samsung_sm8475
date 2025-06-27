398ebdc37b5e30198cd44b6678166193e268d191
Flopster101 <nahuelgomez329@gmail.com>
2025-06-27 10:08:10 -0300

    proc: Heavily limit Samsung's snap service CPU usage (v2)
    This "vendor.samsung.hardware.snap-service" process wakes up randomly and
    absolutely hammers a random group of cores for a few minutes or indefinitely,
    causing a lot of power drain and slowing down the device significantly.
    
    It's related to camera AI functions and most of the time seems to wake up
    around 1 minute after the camera app is closed.
    
    I haven't profiled it to see what it's doing with that much CPU power, so
    this hack will keep it limited for now.
    
    * This version of the hack does it directly at the scheduler level.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -57,6 +57,27 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_stat_blocked);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 EXPORT_SYMBOL_GPL(runqueues);
 
+static void check_and_limit_snap_service(struct task_struct *p)
+{
+	static const char snap_service_comm[] = "vendor.samsung.hardware.snap-service";
+	static const struct cpumask snap_service_cpumask = {
+		// This sets the bit for CPU5.
+		.bits = { [0] = (1UL << 5) }
+	};
+
+	// This is a fast check. If the names don't match, we return immediately.
+	if (strcmp(p->comm, snap_service_comm) != 0)
+		return;
+	
+	if (task_nice(p) != 19) {
+		set_user_nice(p, 19);
+	}
+
+	if (!cpumask_equal(&p->cpus_mask, &snap_service_cpumask)) {
+		set_cpus_allowed_ptr(p, &snap_service_cpumask);
+	}
+}
+
 #ifdef CONFIG_SCHED_DEBUG
 /*
  * Debugging: various feature bits
@@ -4253,6 +4274,8 @@ void scheduler_tick(void)
 	struct task_struct *curr = rq->curr;
 	struct rq_flags rf;
 	unsigned long thermal_pressure;
+
+	check_and_limit_snap_service(curr);
 
 	arch_scale_freq_tick();
 	sched_clock_tick();
