18cc16ce036b3e14c63525e16be778d516505311
Flopster101 <nahuelgomez329@gmail.com>
2026-07-03 11:03:36 -0300

    mm: vmscan: Add missing calls to update active/inactive LRU mapping
    Missed back in [1].
    
    [1] 435928c9d867 ("mm/lru_gen: update memcg LRU size")
    
    Was causing this WARN to appear:
    
    [   47.461881] [7:      kswapd0:1:  155] ------------[ cut here ]------------
    [   47.461893] [7:      kswapd0:1:  155] mem_cgroup_update_lru_size(00000000bc691614, 2, -1): lru_size -1
    [   47.461919] [7:      kswapd0:1:  155] WARNING: CPU: 7 PID: 155 at mm/memcontrol.c:1514 mem_cgroup_update_lru_size+0xc8/0xe0
    ...
    [   47.462231] [7:      kswapd0:1:  155] CPU: 7 PID: 155 Comm: kswapd0:1 Tainted: G         C        5.10.257-Floppy-v6.3.2-KN-g2066aad775f3 #1060
    [   47.462233] [7:      kswapd0:1:  155] Hardware name: Samsung M34X EUR OPEN based on S5E8825 (DT)
    [   47.462237] [7:      kswapd0:1:  155] pstate: 60c00085 (nZCv daIf +PAN +UAO -TCO BTYPE=--)
    [   47.462241] [7:      kswapd0:1:  155] pc : mem_cgroup_update_lru_size+0xc8/0xe0
    [   47.462244] [7:      kswapd0:1:  155] lr : mem_cgroup_update_lru_size+0xc0/0xe0
    [   47.462246] [7:      kswapd0:1:  155] sp : ffffffc00abe3860
    [   47.462247] [7:      kswapd0:1:  155] x29: ffffffc00abe3860 x28: 0000000000001000
    [   47.462250] [7:      kswapd0:1:  155] x27: 0000000000000001 x26: 5e00000000000056
    [   47.462253] [7:      kswapd0:1:  155] x25: ffffffff22bed080 x24: ffffffff22bed088
    [   47.462256] [7:      kswapd0:1:  155] x23: 0000000000000003 x22: ffffffc009ccb680
    [   47.462258] [7:      kswapd0:1:  155] x21: ffffff8062403620 x20: 00000000ffffffff
    [   47.462261] [7:      kswapd0:1:  155] x19: 0000000000000002 x18: ffffffc00abfd000
    [   47.462264] [7:      kswapd0:1:  155] x17: 0000000000000001 x16: ffffffffffffffff
    [   47.462266] [7:      kswapd0:1:  155] x15: 0000000000000003 x14: 0000000000000003
    [   47.462269] [7:      kswapd0:1:  155] x13: ffffffc009aebd40 x12: 0000000000000003
    [   47.462271] [7:      kswapd0:1:  155] x11: 00000000ffffffff x10: 0000000100000002
    [   47.462274] [7:      kswapd0:1:  155] x9 : 3cb78977f9b98500 x8 : 3cb78977f9b98500
    [   47.462277] [7:      kswapd0:1:  155] x7 : 6c203a29312d202c x6 : ffffffc00a523aaa
    [   47.462279] [7:      kswapd0:1:  155] x5 : 0000000000000000 x4 : 0000000000000007
    [   47.462282] [7:      kswapd0:1:  155] x3 : 00000000ffffffff x2 : 0000000000000000
    [   47.462284] [7:      kswapd0:1:  155] x1 : 0000000000000002 x0 : 0000000000000040
    [   47.462287] [7:      kswapd0:1:  155] Call trace:
    [   47.462290] [7:      kswapd0:1:  155]  mem_cgroup_update_lru_size+0xc8/0xe0
    [   47.462294] [7:      kswapd0:1:  155]  lru_gen_update_size+0x260/0x280
    [   47.462297] [7:      kswapd0:1:  155]  shrink_lruvec+0xf18/0x3200
    [   47.462300] [7:      kswapd0:1:  155]  shrink_node+0x110/0x980
    [   47.462302] [7:      kswapd0:1:  155]  kswapd+0xcec/0x1420
    [   47.462307] [7:      kswapd0:1:  155]  kthread+0x144/0x170
    [   47.462311] [7:      kswapd0:1:  155]  ret_from_fork+0x10/0x18
    [   47.462313] [7:      kswapd0:1:  155] ---[ end trace 3f1d4d834260716f ]---
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3529,6 +3529,9 @@ static void reset_batch_size(struct lruvec *lruvec, struct lru_gen_mm_walk *walk
 		if (lru_gen_is_active(lruvec, gen))
 			lru += LRU_ACTIVE;
 		__update_lru_size(lruvec, lru, zone, delta);
+#ifdef CONFIG_MEMCG
+		mem_cgroup_update_lru_size(lruvec, lru, zone, delta);
+#endif
 	}
 }
 
@@ -4162,6 +4165,10 @@ restart:
 
 			__update_lru_size(lruvec, lru, zone, delta);
 			__update_lru_size(lruvec, lru + LRU_ACTIVE, zone, -delta);
+#ifdef CONFIG_MEMCG
+			mem_cgroup_update_lru_size(lruvec, lru, zone, delta);
+			mem_cgroup_update_lru_size(lruvec, lru + LRU_ACTIVE, zone, -delta);
+#endif
 		}
 	}
 
