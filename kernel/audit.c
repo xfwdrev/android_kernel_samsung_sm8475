8c3f09f7668c3e967629d8e240172801a1701897
ExtremeXT <extremext@lineageos.org>
2026-07-10 22:41:22 +0300

    fs: audit: Add missing AVC logic
    Change-Id: I440c5ba6c137ebf700503e81718981f8637a2440
    Signed-off-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: milxnaq <dupazlasu66@gmail.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -545,8 +545,15 @@ static void kauditd_printk_skb(struct sk_buff *skb)
 	struct nlmsghdr *nlh = nlmsg_hdr(skb);
 	char *data = nlmsg_data(nlh);
 
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_PROC_AVC
+	if (nlh->nlmsg_type != AUDIT_EOE && nlh->nlmsg_type != AUDIT_NETFILTER_CFG)
+		sec_avc_log("%s\n", data);
+#else
 	if (nlh->nlmsg_type != AUDIT_EOE && printk_ratelimit())
 		pr_notice("type=%d %s\n", nlh->nlmsg_type, data);
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 }
 
 /**
@@ -788,6 +795,15 @@ retry:
 			} else
 				goto retry;
 		} else {
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_PROC_AVC
+			struct nlmsghdr *nlh = nlmsg_hdr(skb);
+			char *data = nlmsg_data(nlh);
+
+			if (nlh->nlmsg_type != AUDIT_EOE && nlh->nlmsg_type != AUDIT_NETFILTER_CFG)
+				sec_avc_log("%s\n", data);
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 			/* skb sent - drop the extra reference and continue */
 			consume_skb(skb);
 			failed = 0;
