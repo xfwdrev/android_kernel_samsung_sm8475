5a275f2d8111ea77a87a2bdc32d5e667ee0f2883
milxnaq <dupazlasu66@gmail.com>
2026-08-14 14:18:39 +0200

    Revert "ANDROID: arm64: add __va_function and __pa_function"
    * Reason for revert: In preparation for CFI backport from android14-6.1 branch.
    
    This reverts commit b1debb369c4bbe597bf03daeef938acc27bd2346.
    
    Change-Id: I8324981404cae102257e23373c3093f6e9d6eafd
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- arch/arm64/include/asm/kvm_asm.h
+++ arch/arm64/include/asm/kvm_asm.h
@@ -173,7 +173,7 @@ struct kvm_nvhe_init_params {
 			val = lm_alias((ptr));				\
 		val;							\
 	 })
-#define kvm_ksym_ref_nvhe(sym)	kvm_ksym_ref(__va_function(kvm_nvhe_sym(sym)))
+#define kvm_ksym_ref_nvhe(sym)	kvm_ksym_ref(kvm_nvhe_sym(sym))
 
 struct kvm;
 struct kvm_vcpu;
